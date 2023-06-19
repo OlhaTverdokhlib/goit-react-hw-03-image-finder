3affabba2db8f58b140e46e1bc19c3b46f4425b5
Olha Tverdokhlib <balabolya@gmail.com>
2023-06-19 11:39:20 +0200

    props deletes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { getPhotoByName } from 'services/api';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
